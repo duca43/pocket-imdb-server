a5800d3a0312052f4017a8474543820697831fd4
duca43 <dmaksic@uns.ac.rs>
2021-01-13 23:27:44 +0100

    refactor user model, add new serializer and create custom user manager

--- pocketimdb/users/admin.py
+++ pocketimdb/users/admin.py
@@ -4,4 +4,24 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'name', 'is_staff')
+    list_filter = ('email', 'name', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    ordering = ('email',)
+    search_fields = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
