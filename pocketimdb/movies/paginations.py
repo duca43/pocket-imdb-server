f7e59e627d5e2d5f25e19f93cb16857383bc36c3
duca43 <dmaksic@uns.ac.rs>
2021-01-18 11:37:24 +0100

    refactored custom pagination response to return total_pages and removed page_size and moved pagination setup into settings.py

--- pocketimdb/movies/paginations.py
+++ pocketimdb/movies/paginations.py
@@ -1,16 +1,12 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-DEFAULT_PAGE = 1
-
 class CustomPageNumberPagination(PageNumberPagination):
-    page = DEFAULT_PAGE
-    page_size = 10
-
+    
     def get_paginated_response(self, data):
         return Response({
             'total': self.page.paginator.count,
-            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
-            'page_size': self.page_size,
+            'total_pages': self.page.paginator.num_pages,
+            'page': self.page.number,
             'results': data
         })
--- pocketimdb/settings.py
+++ pocketimdb/settings.py
@@ -138,7 +138,9 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'pocketimdb.movies.paginations.CustomPageNumberPagination',
+    'PAGE_SIZE': 10
 } 
 
 SIMPLE_JWT = {
