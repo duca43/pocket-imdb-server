5f5fb456e207426090b916f83cc3f2fa4b660cd9
duca43 <dusan.maksic@vivifyideas.com>
2021-01-22 10:57:53 +0100

    simplified increment_visits method in MovieViewSet with using get instead of filter method on Movie queryset

--- pocketimdb/movies/views.py
+++ pocketimdb/movies/views.py
@@ -19,9 +19,7 @@ class MovieViewSet(mixins.ListModelMixin,
 
     @action(methods=['PATCH'], detail=True, url_path='visits') 
     def increment_visits(self, request, pk):          
-        movies = Movie.objects.filter(pk=pk)
-        if len(movies) == 0:
-            error = {"not_exists_error": ["There is no movie with given id."]}
-            return Response(error, status=HTTP_400_BAD_REQUEST)
-        movies.update(visits=F('visits') + 1)
+        movie = Movie.objects.get(pk=pk)
+        movie.visits = F('visits') + 1
+        movie.save()
         return Response(status=HTTP_200_OK)
